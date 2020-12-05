ff7ed7ca10a0f8fdda6faad90644328a6a04176e
Aman Shah <amanshah2711@berkeley.edu>
2020-12-04 21:17:38 -0500

    configs

--- rlproj/config/default.py
+++ rlproj/config/default.py
@@ -2,6 +2,25 @@ from yacs.config import CfgNode as CN
 
 _C = CN()
 
+_C.ROOT_DIR = ''
+_C.EXP_NAME = 'default'
+_C.SEED = 0
+_C.CHECKPOINT_FREQ = 5000 #TODO PICK A REASONABLE VALUE
+_C.LOGGING_FREQ = 5000 #TODO PICK A REASONABLE VALUE
+_C.DEBUG = False
+
+_C.ENVIRONMENT = CN()
+_C.ENVIRONMENT.NAME = ''
+_C.ENVIRONMENT.NUM_ENV = ''
+_C.ENVIRONMENT.TOTAL_TIMESTEPS = ''
+
+_C.RL = CN()
+_C.RL.ALGO = 'ppo'
+_C.RL.POLICY = 'mlppolicy'
+_C.RL.BATCH_SIZE = 2048
+_C.RL.LR = 3e-4
+_C.RL.ARGS = '{}'
+
 
 
 def update_specific_config(cfg, name):
---
+++ scripts/train.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+
+from rlproj import utils
+
+
+import tensorflow as tf
+import stable_baselines
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
